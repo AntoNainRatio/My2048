ecb9b7c6afac3685a34ee1c457584a8202d0fdb5
Antonin Lhuillery <antonin.lhuillery@epita.fr>
2024-10-22 19:11:00 +0200

    clanged

--- Game/src/board.c
+++ Game/src/board.c
@@ -1,232 +1,235 @@
+#include "../include/board.h"
+
+#include <cairo.h>
+#include <gtk/gtk.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
-#include <cairo.h>
-#include <gtk/gtk.h>
-#include "../include/board.h"
 
-int* getBoard(int n)
+int *getBoard(int n)
 {
-	int* res = calloc(n*n,sizeof(int));
-	return res;
+    int *res = calloc(n * n, sizeof(int));
+    return res;
 }
 
-int mul(int* a, int* b)
+int mul(int *a, int *b)
 {
-	int res = 0;
-	for(int y = 0; y < SIZE; y++)
-	{
-		for(int x = 0; x < SIZE; x++)
-		{
-			res += a[y * SIZE +x] * b[y * SIZE + x];
-		}
-	}
-	return res;
+    int res = 0;
+    for (int y = 0; y < SIZE; y++)
+    {
+        for (int x = 0; x < SIZE; x++)
+        {
+            res += a[y * SIZE + x] * b[y * SIZE + x];
+        }
+    }
+    return res;
 }
 
-int* getCopy(int* b)
+int *getCopy(int *b)
 {
-	int* res = malloc(SIZE*SIZE*sizeof(int));
-	for(int y = 0; y < SIZE; y++)
-	{
-		for(int x = 0; x < SIZE; x++)
-		{
-			res[y * SIZE + x] = b[y * SIZE + x];
-		}
-	}
-	return res;
+    int *res = malloc(SIZE * SIZE * sizeof(int));
+    for (int y = 0; y < SIZE; y++)
+    {
+        for (int x = 0; x < SIZE; x++)
+        {
+            res[y * SIZE + x] = b[y * SIZE + x];
+        }
+    }
+    return res;
 }
 
-void putNewValue(int* board)
+void putNewValue(int *board)
 {
-	int x = rand() % SIZE;
-	int y = rand() % SIZE;
-	while(board[y*SIZE+x] != 0)
-	{
-		x=rand() % SIZE;
-		y=rand() % SIZE;
-	}
-	int new = rand()%10;
-	board[y*SIZE+x]=(new > 0 ? 1 : 2)*2;
+    int x = rand() % SIZE;
+    int y = rand() % SIZE;
+    while (board[y * SIZE + x] != 0)
+    {
+        x = rand() % SIZE;
+        y = rand() % SIZE;
+    }
+    int new = rand() % 10;
+    board[y * SIZE + x] = (new > 0 ? 1 : 2) * 2;
 }
 
-int getNbVide(int* b)
+int getNbVide(int *b)
 {
-	int res = 0;
-	for(int y = 0; y < SIZE; y++)
-	{
-		for(int x = 0; x < SIZE; x++)
-		{
-			if(b[y * SIZE + x] == 0)
-			{
-				res++;
-			}
-		}
-	}
-	return res;
+    int res = 0;
+    for (int y = 0; y < SIZE; y++)
+    {
+        for (int x = 0; x < SIZE; x++)
+        {
+            if (b[y * SIZE + x] == 0)
+            {
+                res++;
+            }
+        }
+    }
+    return res;
 }
 
-int isFull(int* b)
+int isFull(int *b)
 {
-	if(getNbVide(b) == 0)
-	{
-		return 0;
-	}
-	return -1;
+    if (getNbVide(b) == 0)
+    {
+        return 0;
+    }
+    return -1;
 }
 
-int possibleMove(int* board)
+int possibleMove(int *board)
 {
-	int n = SIZE;
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			if(board[i*n+j] == 0)
-			{
-				return 1;
-			}
-			if(i+1 < n && board[(i+1)*n+j] == board[i*n+j])
-			{
-				return 1;
-			}
-			if(i-1 >= 0 && board[(i-1)*n+j] == board[i*n+j])
-			{
-				return 1;
-			}
-			if(j+1 < n && board[i*n + j+1] == board[i*n+j])
-			{
-				return 1;
-			}
-			if(j-1 >= 0 && board[i*n+j-1] == board[i*n+j])
-			{
-				return 1;
-			}
-		}
-	}
-	return 0;
+    int n = SIZE;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (board[i * n + j] == 0)
+            {
+                return 1;
+            }
+            if (i + 1 < n && board[(i + 1) * n + j] == board[i * n + j])
+            {
+                return 1;
+            }
+            if (i - 1 >= 0 && board[(i - 1) * n + j] == board[i * n + j])
+            {
+                return 1;
+            }
+            if (j + 1 < n && board[i * n + j + 1] == board[i * n + j])
+            {
+                return 1;
+            }
+            if (j - 1 >= 0 && board[i * n + j - 1] == board[i * n + j])
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 
-void drawBoard(int* b, int screenWidth, int screenHeight, cairo_t *cr)
+void drawBoard(int *b, int screenWidth, int screenHeight, cairo_t *cr)
 {
-	cairo_set_source_rgba(cr, 0.73, 0.69, 0.63, 1);
-	cairo_rectangle(cr, 0, 0, screenWidth, screenHeight);
-	cairo_fill(cr);
-	int screenSize;
-	if(screenWidth < screenHeight)
-	{
-		screenSize = screenWidth;
-	}
-	else
-	{
-		screenSize = screenHeight;
-	}
+    cairo_set_source_rgba(cr, 0.73, 0.69, 0.63, 1);
+    cairo_rectangle(cr, 0, 0, screenWidth, screenHeight);
+    cairo_fill(cr);
+    int screenSize;
+    if (screenWidth < screenHeight)
+    {
+        screenSize = screenWidth;
+    }
+    else
+    {
+        screenSize = screenHeight;
+    }
 
-	int borderSize = screenSize / 100;
-	int tileSize = screenSize / 4.2;
+    int borderSize = screenSize / 100;
+    int tileSize = screenSize / 4.2;
 
-	int yScreen = borderSize;
+    int yScreen = borderSize;
 
-	for(int y = 0; y < SIZE; y++)
-	{
-		int xScreen = borderSize;
-		for(int x = 0; x < SIZE; x++)
-		{
-			int p = 1;
-			int n = 0;
-			while(p < b[y * SIZE + x])
-			{
-				p = p * 2;
-				n++;
-			}
-			switch(n)
-			{
-				case(0):
-					cairo_set_source_rgba(cr, 0.80, 0.76, 0.71, 1);
-					break;
-				case(1):
-					cairo_set_source_rgba(cr, 0.93, 0.89, 0.85, 1);
-					break;
-				case(2):
-					cairo_set_source_rgba(cr, 0.93, 0.88, 0.78, 1);
-					break;
-				case(3):
-					cairo_set_source_rgba(cr, 0.95, 0.70, 0.47, 1);
-					break;
-				case(4):
-					cairo_set_source_rgba(cr, 0.96, 0.58, 0.39,1);
-					break;
-				case(5):
-					cairo_set_source_rgba(cr, 0.96, 0.49, 0.37, 1);
-					break;
-				case(6):
-					cairo_set_source_rgba(cr, 0.96, 0.37, 0.23, 1);
-					break;
-				case(7):
-					cairo_set_source_rgba(cr, 0.96, 0.81, 0.44, 1);
-					break;
-				case(8):
-					cairo_set_source_rgba(cr, 0.93, 0.80, 0.38, 1);
-					break;
-				case(9):
-					cairo_set_source_rgba(cr, 0.93, 0.78, 0.33, 1);
-					break;
-				case(10):
-					cairo_set_source_rgba(cr, 0.93, 0.77, 0.24, 1);
-					break;
-				case(11):
-					cairo_set_source_rgba(cr, 0.93, 0.76, 0.18, 1);
-					break;
-				case(12):
-					cairo_set_source_rgba(cr, 0.24, 0.23, 0.20, 1);
-				default:
-					cairo_set_source_rgba(cr, 0, 0, 0, 1);
-					break;
-			}
-			cairo_rectangle(cr, xScreen, yScreen, tileSize, tileSize);
-			cairo_fill(cr);
+    for (int y = 0; y < SIZE; y++)
+    {
+        int xScreen = borderSize;
+        for (int x = 0; x < SIZE; x++)
+        {
+            int p = 1;
+            int n = 0;
+            while (p < b[y * SIZE + x])
+            {
+                p = p * 2;
+                n++;
+            }
+            switch (n)
+            {
+            case (0):
+                cairo_set_source_rgba(cr, 0.80, 0.76, 0.71, 1);
+                break;
+            case (1):
+                cairo_set_source_rgba(cr, 0.93, 0.89, 0.85, 1);
+                break;
+            case (2):
+                cairo_set_source_rgba(cr, 0.93, 0.88, 0.78, 1);
+                break;
+            case (3):
+                cairo_set_source_rgba(cr, 0.95, 0.70, 0.47, 1);
+                break;
+            case (4):
+                cairo_set_source_rgba(cr, 0.96, 0.58, 0.39, 1);
+                break;
+            case (5):
+                cairo_set_source_rgba(cr, 0.96, 0.49, 0.37, 1);
+                break;
+            case (6):
+                cairo_set_source_rgba(cr, 0.96, 0.37, 0.23, 1);
+                break;
+            case (7):
+                cairo_set_source_rgba(cr, 0.96, 0.81, 0.44, 1);
+                break;
+            case (8):
+                cairo_set_source_rgba(cr, 0.93, 0.80, 0.38, 1);
+                break;
+            case (9):
+                cairo_set_source_rgba(cr, 0.93, 0.78, 0.33, 1);
+                break;
+            case (10):
+                cairo_set_source_rgba(cr, 0.93, 0.77, 0.24, 1);
+                break;
+            case (11):
+                cairo_set_source_rgba(cr, 0.93, 0.76, 0.18, 1);
+                break;
+            case (12):
+                cairo_set_source_rgba(cr, 0.24, 0.23, 0.20, 1);
+            default:
+                cairo_set_source_rgba(cr, 0, 0, 0, 1);
+                break;
+            }
+            cairo_rectangle(cr, xScreen, yScreen, tileSize, tileSize);
+            cairo_fill(cr);
 
-			if(b[y * SIZE + x] > 0)
-			{
-				char str[8];
-				snprintf(str, sizeof(str), "%d", b[y * SIZE + x]);
+            if (b[y * SIZE + x] > 0)
+            {
+                char str[8];
+                snprintf(str, sizeof(str), "%d", b[y * SIZE + x]);
 
-				int v = (n > 2) ? 1 : 0.8;
-				cairo_set_source_rgba(cr, v, v, v, 1);
+                int v = (n > 2) ? 1 : 0.8;
+                cairo_set_source_rgba(cr, v, v, v, 1);
 
-				cairo_set_font_size(cr, tileSize / 4);
+                cairo_set_font_size(cr, tileSize / 4);
 
-				cairo_text_extents_t extents;
-				cairo_text_extents(cr, str, &extents); // Obtenir les dimensions du texte
-				double xText = xScreen + (tileSize - extents.width) / 2 - extents.x_bearing;
-                double yText = yScreen + (tileSize - extents.height) / 2 - extents.y_bearing;
+                cairo_text_extents_t extents;
+                cairo_text_extents(cr, str,
+                                   &extents); // Obtenir les dimensions du texte
+                double xText = xScreen + (tileSize - extents.width) / 2
+                    - extents.x_bearing;
+                double yText = yScreen + (tileSize - extents.height) / 2
+                    - extents.y_bearing;
 
                 // Dessiner le texte au centre de la tuile
                 cairo_move_to(cr, xText, yText);
                 cairo_show_text(cr, str);
+            }
 
-			}
-
-			xScreen += tileSize + borderSize;
-		}
-		yScreen += tileSize + borderSize;
-	}
+            xScreen += tileSize + borderSize;
+        }
+        yScreen += tileSize + borderSize;
+    }
 }
 
-void printBoard(int* b)
+void printBoard(int *b)
 {
-	for(int y = 0; y < SIZE; y++)
-	{
-		for(int x = 0; x < SIZE; x++)
-		{
-			g_print("%d ",b[y * SIZE + x]);
-		}
-		g_print("\n");
-	}
+    for (int y = 0; y < SIZE; y++)
+    {
+        for (int x = 0; x < SIZE; x++)
+        {
+            g_print("%d ", b[y * SIZE + x]);
+        }
+        g_print("\n");
+    }
 }
 
-void freeBoard(int* b)
+void freeBoard(int *b)
 {
-	free(b);
+    free(b);
 }
--- Game/src/bot.c
+++ Game/src/bot.c
@@ -1,356 +1,335 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "../include/board.h"
-#include "../include/control.h"
 #include "../include/bot.h"
 
-int bottomLeft[SIZE*SIZE] = 
-{
-	2, 4, 8, 16,
-	256, 128, 64, 32,
-	512, 1024, 2048, 4096,
-	65536, 32768, 16384, 8192
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/board.h"
+#include "../include/control.h"
+
+int bottomLeft[SIZE * SIZE] = {
+    2,   4,    8,    16,   256,   128,   64,    32,
+    512, 1024, 2048, 4096, 65536, 32768, 16384, 8192
 };
 
-int bottomRight[SIZE*SIZE] =
+int bottomRight[SIZE * SIZE] = { 16,   8,     4,     2,    32,   64,
+                                 128,  256,   4096,  2048, 1024, 512,
+                                 8192, 16384, 32768, 65536 };
+
+int topRight[SIZE * SIZE] = { 8192, 16384, 32768, 65536, 4096, 2048, 1024, 512,
+                              32,   64,    128,   256,   16,   8,    4,    2 };
+
+int topLeft[SIZE * SIZE] = { 65536, 32768, 16384, 8192, 512, 1024, 2048, 4096,
+                             256,   128,   64,    32,   2,   4,    8,    16 };
+
+int getMax(int *b)
 {
-	16, 8, 4, 2,
-	32, 64, 128, 256,
-	4096, 2048, 1024,512,
-	8192, 16384, 32768, 65536
-};
+    int res = mul(b, bottomLeft);
 
-int topRight[SIZE*SIZE] =
-{
-	8192, 16384, 32768, 65536,
-	4096, 2048, 1024, 512,
-	32, 64, 128, 256,
-	16, 8, 4, 2
-};
+    /*int tmp1 = mul(b,bottomRight);
+    if(tmp1 > res)
+    {
+        res = tmp1;
+    }
 
-int topLeft[SIZE*SIZE] =
-{
-	65536, 32768, 16384, 8192,
-	512, 1024, 2048, 4096,
-	256, 128, 64, 32,
-	2, 4, 8, 16
-};
+    int tmp2 = mul(b,topRight);
+    if(tmp2 > res)
+    {
+        res = tmp2;
+    }
 
-int getMax(int* b)
-{
-	int res = mul(b,bottomLeft);
+    int tmp3 = mul(b, topLeft);
+    if(tmp3 > res)
+    {
+        res = tmp3;
+    }*/
 
-	/*int tmp1 = mul(b,bottomRight);
-	if(tmp1 > res)
-	{
-		res = tmp1;
-	}
-
-	int tmp2 = mul(b,topRight);
-	if(tmp2 > res)
-	{
-		res = tmp2;
-	}
-
-	int tmp3 = mul(b, topLeft);
-	if(tmp3 > res)
-	{
-		res = tmp3;
-	}*/
-
-	return res;
+    return res;
 }
 
-
-
-int* getSpawns(int* b, int* l)
+int *getSpawns(int *b, int *l)
 {
-	int* res = malloc(16*2*sizeof(int));
-	int n = 0;
-	for(int y = 0; y < SIZE; y++)
-	{
-		for(int x = 0; x < SIZE; x++)
-		{
-			if(b[y * SIZE + x] == 0)
-			{
-				res[n] = x;
-				res[n+1] = y;
-				n += 2;
-			}
-			else
-			{
-				res[y * SIZE + x] = 1;
-			}
-		}
-	}
-	*l = (int)(n / 2);
-	res = realloc(res, n*sizeof(int));
-	return res;
+    int *res = malloc(16 * 2 * sizeof(int));
+    int n = 0;
+    for (int y = 0; y < SIZE; y++)
+    {
+        for (int x = 0; x < SIZE; x++)
+        {
+            if (b[y * SIZE + x] == 0)
+            {
+                res[n] = x;
+                res[n + 1] = y;
+                n += 2;
+            }
+            else
+            {
+                res[y * SIZE + x] = 1;
+            }
+        }
+    }
+    *l = (int)(n / 2);
+    res = realloc(res, n * sizeof(int));
+    return res;
 }
 
-
-
-
-long double rec_depth(int* b, long double curr, int depth)
+long double rec_depth(int *b, long double curr, int depth)
 {
-	if(depth == 0)
-	{
-		return curr;
-	}
-	else
-	{
-		long double res = 0;
-		int n;
+    if (depth == 0)
+    {
+        return curr;
+    }
+    else
+    {
+        long double res = 0;
+        int n;
 
-		// UP
+        // UP
 
-		int* up = getCopy(b);
-		int vUp = moveUp(up);
-		if(vUp != -1)
-		{
-			int* spawns = getSpawns(up, &n);
-			
-			for(int i = 0; i < n*2; i += 2)
-			{
-				int* tmpB = getCopy(up);
-				tmpB[ (spawns[i+1]) * SIZE + (spawns[i])] = 2;
-				//g_print("preProba = %Lf\n",preProba);
-				res += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
-				free(tmpB);
+        int *up = getCopy(b);
+        int vUp = moveUp(up);
+        if (vUp != -1)
+        {
+            int *spawns = getSpawns(up, &n);
 
-				int* tmpB1 = getCopy(up);
-				tmpB1[ (spawns[i+1]) * SIZE + (spawns[i]) ] = 4;
-				res += rec_depth(tmpB1, curr + (getMax(tmpB1) * 0.1), depth - 1);
-				free(tmpB1);
-			}
-			free(spawns);
-		}
-		free(up);
+            for (int i = 0; i < n * 2; i += 2)
+            {
+                int *tmpB = getCopy(up);
+                tmpB[(spawns[i + 1]) * SIZE + (spawns[i])] = 2;
+                // g_print("preProba = %Lf\n",preProba);
+                res += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
+                free(tmpB);
 
-			// DOWN
+                int *tmpB1 = getCopy(up);
+                tmpB1[(spawns[i + 1]) * SIZE + (spawns[i])] = 4;
+                res +=
+                    rec_depth(tmpB1, curr + (getMax(tmpB1) * 0.1), depth - 1);
+                free(tmpB1);
+            }
+            free(spawns);
+        }
+        free(up);
 
-		int* down = getCopy(b);
-		int vDown = moveDown(down);
-		if(vDown != -1)
-		{
-			int* spawns = getSpawns(down, &n);
-			
-			for(int i = 0; i < n*2; i += 2)
-			{
-				int* tmpB = getCopy(down);
-				tmpB[ (spawns[i+1]) * SIZE + (spawns[i])] = 2;
-				res += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
-				free(tmpB);
+        // DOWN
 
-				int* tmpB1 = getCopy(down);
-				tmpB1[ (spawns[i+1]) * SIZE + (spawns[i]) ] = 4;
-				res += rec_depth(tmpB1, curr + (getMax(tmpB1) * 0.1), depth - 1);
-				free(tmpB1);
-			}
-			free(spawns);
-		}
-		free(down);
+        int *down = getCopy(b);
+        int vDown = moveDown(down);
+        if (vDown != -1)
+        {
+            int *spawns = getSpawns(down, &n);
 
-		// LEFT
+            for (int i = 0; i < n * 2; i += 2)
+            {
+                int *tmpB = getCopy(down);
+                tmpB[(spawns[i + 1]) * SIZE + (spawns[i])] = 2;
+                res += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
+                free(tmpB);
 
-		int* left = getCopy(b);
-		int vLeft = moveLeft(left);
-		if(vLeft != -1)
-		{
-			int* spawns = getSpawns(left, &n);
-			
-			for(int i = 0; i < n*2; i += 2)
-			{
-				int* tmpB = getCopy(left);
-				tmpB[ (spawns[i+1]) * SIZE + (spawns[i])] = 2;
-				res += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
-				free(tmpB);
+                int *tmpB1 = getCopy(down);
+                tmpB1[(spawns[i + 1]) * SIZE + (spawns[i])] = 4;
+                res +=
+                    rec_depth(tmpB1, curr + (getMax(tmpB1) * 0.1), depth - 1);
+                free(tmpB1);
+            }
+            free(spawns);
+        }
+        free(down);
 
-				int* tmpB1 = getCopy(left);
-				tmpB1[ (spawns[i+1]) * SIZE + (spawns[i]) ] = 4;
-                res += rec_depth(tmpB1, curr + (getMax(tmpB1) * 0.1), depth - 1);
-				free(tmpB1);
-			}
-			free(spawns);
-		}
-		free(left);
+        // LEFT
 
-		// RIGHT
+        int *left = getCopy(b);
+        int vLeft = moveLeft(left);
+        if (vLeft != -1)
+        {
+            int *spawns = getSpawns(left, &n);
 
-		int* right = getCopy(b);
-		int vRight = moveRight(right);
-		if(vRight != -1)
-		{
-			int* spawns = getSpawns(right, &n);
-			
-			for(int i = 0; i < n*2; i += 2)
-			{
-				int* tmpB = getCopy(right);
-				tmpB[ (spawns[i+1]) * SIZE + (spawns[i])] = 2;
-				res += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
-				free(tmpB);
+            for (int i = 0; i < n * 2; i += 2)
+            {
+                int *tmpB = getCopy(left);
+                tmpB[(spawns[i + 1]) * SIZE + (spawns[i])] = 2;
+                res += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
+                free(tmpB);
 
-				int* tmpB1 = getCopy(right);
-				tmpB1[ (spawns[i+1]) * SIZE + (spawns[i]) ] = 4;
-				res += rec_depth(tmpB1, curr + getMax(tmpB1) * 0.1, depth -1);
-				free(tmpB1);
-			}
-			free(spawns);
-		}
-		free(right);
+                int *tmpB1 = getCopy(left);
+                tmpB1[(spawns[i + 1]) * SIZE + (spawns[i])] = 4;
+                res +=
+                    rec_depth(tmpB1, curr + (getMax(tmpB1) * 0.1), depth - 1);
+                free(tmpB1);
+            }
+            free(spawns);
+        }
+        free(left);
 
-		return res;
-	}
+        // RIGHT
+
+        int *right = getCopy(b);
+        int vRight = moveRight(right);
+        if (vRight != -1)
+        {
+            int *spawns = getSpawns(right, &n);
+
+            for (int i = 0; i < n * 2; i += 2)
+            {
+                int *tmpB = getCopy(right);
+                tmpB[(spawns[i + 1]) * SIZE + (spawns[i])] = 2;
+                res += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
+                free(tmpB);
+
+                int *tmpB1 = getCopy(right);
+                tmpB1[(spawns[i + 1]) * SIZE + (spawns[i])] = 4;
+                res += rec_depth(tmpB1, curr + getMax(tmpB1) * 0.1, depth - 1);
+                free(tmpB1);
+            }
+            free(spawns);
+        }
+        free(right);
+
+        return res;
+    }
 }
 
-
-
-
-long double* chapo(int* b)
+long double *chapo(int *b)
 {
-	long double* res = calloc(4,sizeof(long double));
-	int depth = 4;
-	long double curr = 0;
-	int n;
+    long double *res = calloc(4, sizeof(long double));
+    int depth = 4;
+    long double curr = 0;
+    int n;
 
-	int* up = getCopy(b);
-	int vUp = moveUp(up);
-	if(vUp != -1)
-	{
-		int* spawns = getSpawns(up, &n);
-		
-		for(int i = 0; i < n*2; i += 2)
-		{
-			int* tmpB = getCopy(up);
-			tmpB[ (spawns[i+1]) * SIZE + (spawns[i])] = 2;
-			res[0] += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
-			free(tmpB);
+    int *up = getCopy(b);
+    int vUp = moveUp(up);
+    if (vUp != -1)
+    {
+        int *spawns = getSpawns(up, &n);
 
-			int* tmpB1 = getCopy(up);
-			tmpB1[ (spawns[i+1]) * SIZE + (spawns[i]) ] = 4;
-			res[0] += rec_depth(tmpB1, curr + (getMax(tmpB1) * 0.1), depth -1);
-			free(tmpB1);
-		}
-		free(spawns);
-	}
-	else
-	{
-		res[0] = -1;
-	}
-	free(up);
+        for (int i = 0; i < n * 2; i += 2)
+        {
+            int *tmpB = getCopy(up);
+            tmpB[(spawns[i + 1]) * SIZE + (spawns[i])] = 2;
+            res[0] += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
+            free(tmpB);
 
-	int* down = getCopy(b);
-	int vDown = moveDown(down);
+            int *tmpB1 = getCopy(up);
+            tmpB1[(spawns[i + 1]) * SIZE + (spawns[i])] = 4;
+            res[0] += rec_depth(tmpB1, curr + (getMax(tmpB1) * 0.1), depth - 1);
+            free(tmpB1);
+        }
+        free(spawns);
+    }
+    else
+    {
+        res[0] = -1;
+    }
+    free(up);
+
+    int *down = getCopy(b);
+    int vDown = moveDown(down);
     curr = 0;
-	if(vDown != -1)
-	{
-		int* spawns = getSpawns(down, &n);
-		
-		for(int i = 0; i < n*2; i += 2)
-		{
-			int* tmpB = getCopy(down);
-			tmpB[ (spawns[i+1]) * SIZE + (spawns[i])] = 2;
-			res[1] += rec_depth(tmpB, curr + ( getMax(tmpB)* 0.9), depth - 1);
-			free(tmpB);
+    if (vDown != -1)
+    {
+        int *spawns = getSpawns(down, &n);
 
-			int* tmpB1 = getCopy(down);
-			tmpB1[ (spawns[i+1]) * SIZE + (spawns[i]) ] = 4;
-			res[1] += rec_depth(tmpB1, curr + getMax(tmpB1) * 0.1, depth -1);
-			free(tmpB1);
-		}
-		free(spawns);
-	}
-	else
-	{
-		res[1] = -1;
-	}
-	free(down);
+        for (int i = 0; i < n * 2; i += 2)
+        {
+            int *tmpB = getCopy(down);
+            tmpB[(spawns[i + 1]) * SIZE + (spawns[i])] = 2;
+            res[1] += rec_depth(tmpB, curr + (getMax(tmpB) * 0.9), depth - 1);
+            free(tmpB);
 
-	int* left = getCopy(b);
-	int vLeft = moveLeft(left);
+            int *tmpB1 = getCopy(down);
+            tmpB1[(spawns[i + 1]) * SIZE + (spawns[i])] = 4;
+            res[1] += rec_depth(tmpB1, curr + getMax(tmpB1) * 0.1, depth - 1);
+            free(tmpB1);
+        }
+        free(spawns);
+    }
+    else
+    {
+        res[1] = -1;
+    }
+    free(down);
+
+    int *left = getCopy(b);
+    int vLeft = moveLeft(left);
     curr = 0;
-	if(vLeft != -1)
-	{
-		int* spawns = getSpawns(left, &n);
-		
-		for(int i = 0; i < n*2; i += 2)
-		{
-			int* tmpB = getCopy(left);
-			tmpB[ (spawns[i+1]) * SIZE + (spawns[i])] = 2;
-			res[2] += rec_depth(tmpB, curr + 0.9 * getMax(tmpB), depth - 1);
-			free(tmpB);
+    if (vLeft != -1)
+    {
+        int *spawns = getSpawns(left, &n);
 
-			int* tmpB1 = getCopy(left);
-			tmpB1[ (spawns[i+1]) * SIZE + (spawns[i]) ] = 4;
-			res[2] += rec_depth(tmpB1, curr + 0.1 * getMax(tmpB1), depth -1);
-			free(tmpB1);
-		}
-		free(spawns);
-	}
-	else
-	{
-		res[2] = -1;
-	}
-	free(left);
+        for (int i = 0; i < n * 2; i += 2)
+        {
+            int *tmpB = getCopy(left);
+            tmpB[(spawns[i + 1]) * SIZE + (spawns[i])] = 2;
+            res[2] += rec_depth(tmpB, curr + 0.9 * getMax(tmpB), depth - 1);
+            free(tmpB);
 
-	int* right = getCopy(b);
-	int vRight = moveRight(right);
+            int *tmpB1 = getCopy(left);
+            tmpB1[(spawns[i + 1]) * SIZE + (spawns[i])] = 4;
+            res[2] += rec_depth(tmpB1, curr + 0.1 * getMax(tmpB1), depth - 1);
+            free(tmpB1);
+        }
+        free(spawns);
+    }
+    else
+    {
+        res[2] = -1;
+    }
+    free(left);
+
+    int *right = getCopy(b);
+    int vRight = moveRight(right);
     curr = 0;
-	if(vRight != -1)
-	{
-		int* spawns = getSpawns(right, &n);
-		
-		for(int i = 0; i < n*2; i += 2)
-		{
-			int* tmpB = getCopy(right);
-			tmpB[ (spawns[i+1]) * SIZE + (spawns[i])] = 2;
-			res[3] += rec_depth(tmpB, curr +0.9 * getMax(tmpB), depth - 1);
-			free(tmpB);
+    if (vRight != -1)
+    {
+        int *spawns = getSpawns(right, &n);
 
-			int* tmpB1 = getCopy(right);
-			tmpB1[ (spawns[i+1]) * SIZE + (spawns[i]) ] = 4;
-			res[3] += rec_depth(tmpB1,curr + getMax(tmpB1) * 0.1, depth -1);
-			free(tmpB1);
-		}
-		free(spawns);
-	}
-	else
-	{
-		res[3] = -1;
-	}
-	free(right);
+        for (int i = 0; i < n * 2; i += 2)
+        {
+            int *tmpB = getCopy(right);
+            tmpB[(spawns[i + 1]) * SIZE + (spawns[i])] = 2;
+            res[3] += rec_depth(tmpB, curr + 0.9 * getMax(tmpB), depth - 1);
+            free(tmpB);
 
+            int *tmpB1 = getCopy(right);
+            tmpB1[(spawns[i + 1]) * SIZE + (spawns[i])] = 4;
+            res[3] += rec_depth(tmpB1, curr + getMax(tmpB1) * 0.1, depth - 1);
+            free(tmpB1);
+        }
+        free(spawns);
+    }
+    else
+    {
+        res[3] = -1;
+    }
+    free(right);
 
-	return res;
+    return res;
 }
 
-void doMove(int* b)
+void doMove(int *b)
 {
-	long double* moveScore = chapo(b);
-	int iMax = 0;
-	int i = 1;
-	while (i < 4)
-	{
-		if(moveScore[i] > moveScore[iMax])
-		{
-			iMax = i;
-		}
-		i++;
-	}
+    long double *moveScore = chapo(b);
+    int iMax = 0;
+    int i = 1;
+    while (i < 4)
+    {
+        if (moveScore[i] > moveScore[iMax])
+        {
+            iMax = i;
+        }
+        i++;
+    }
 
-	switch(iMax)
-	{
-		case 0:
-			moveUp(b);
-			break;
-		case 1:
-			moveDown(b);
-			break;
-		case 2:
-			moveLeft(b);
-			break;
-		default:
-			moveRight(b);
-			break;
-	}
+    switch (iMax)
+    {
+    case 0:
+        moveUp(b);
+        break;
+    case 1:
+        moveDown(b);
+        break;
+    case 2:
+        moveLeft(b);
+        break;
+    default:
+        moveRight(b);
+        break;
+    }
 }
