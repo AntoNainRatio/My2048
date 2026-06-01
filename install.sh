5f5b040fdd321573bd0fe76e972c015e535d39c8
antonin.lhuillery <antonin.lhuillery@epita.fr>
2026-06-01 22:47:42 +0200

    fix: dependencies installation

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
-# Simple Makefile rule to build using a Makefile
+#!/bin/bash
+
+set -e
+
+sudo apt update
+
+sudo apt install -y build-essential pkg-config
+
+sudo apt install -y libgtk-3-dev
+
+sudo apt install -y libcriterion-dev
+
 make all
